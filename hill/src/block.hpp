e3defdf9859fe1a2cb661fb3a216f17493291dde
Arild Hystad <arild.hystad@gmail.com>
2025-04-28 23:01:46 +0200

    Block into separate file

---
+++ hill/src/block.hpp
@@ -0,0 +1,76 @@
+#ifndef BLOCK_HPP_INCLUDED
+#define BLOCK_HPP_INCLUDED
+
+namespace hill {
+
+	struct frame_def { // Actual data storage
+		std::vector<std::uint64_t> data;
+		size_t alloc(size_t n)
+		{
+			auto ix = data.size();
+			for (size_t i=0; i<n; ++i) data.push_back(0);
+			return ix;
+		}
+		void push(std::uint64_t v)
+		{
+			data.push_back(v);
+		}
+	};
+
+	struct val { // Data ref
+		frame_def *frame;
+		size_t frame_ix;
+		const type_desc *type;
+	};
+
+	struct scope { // The names and values
+		std::map<std::string, val> ids;
+		frame_def frame;
+	};
+
+	struct block { // The code
+		scope scope;
+		std::vector<instr> instrs;
+
+		void add(const token &t)
+		{
+			auto instr = make_instr(t);
+
+			switch (t.get_arity()) {
+			case tt_arity::NULLARY:
+				break;
+			case tt_arity::LUNARY: throw not_implemented_exception();
+			case tt_arity::RUNARY: throw not_implemented_exception();
+			case tt_arity::BINARY:
+				if (instrs.size()<2) throw semantic_error_exception();
+				size_t left_ix = instrs.size()-2;
+				size_t right_ix = instrs.size()-1;
+				const auto &res_type = convert_binary(
+					t.get_type(),
+					instrs[left_ix].type,
+					instrs[right_ix].type);
+
+				if (instrs[left_ix].kind==instr_kind::ID) {
+					if (t.get_type()==tt::OP_COLON_EQ) {
+						if (scope.ids.contains(t.get_text())) throw semantic_error_exception();
+
+						val v = {
+							.frame = &scope.frame,
+							.frame_ix = scope.frame.alloc(1), // TODO: Handle larger objects
+							.type = &res_type,
+						};
+						scope.ids[t.get_text()] = v;
+					}
+				} else throw semantic_error_exception();
+				
+				instr.type = res_type;
+
+				break;
+			}
+
+			instrs.push_back(instr);
+		}
+	};
+}
+
+#endif
