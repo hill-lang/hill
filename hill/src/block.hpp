fc1c64ad04ef16fda6d1c77c60c2252021740b90
Arild Hystad <arild.hystad@gmail.com>
2025-05-02 20:08:55 +0200

    wip

--- hill/src/block.hpp
+++ hill/src/block.hpp
@@ -5,6 +5,9 @@ namespace hill {
 
 	struct val_ref {
 		// TODO: Refer to literal_values, og calculated ones
+		bool literal_ref;
+		size_t ix;
+		data_type dt;
 	};
 
 	struct scope { // The names and values
@@ -36,6 +39,18 @@ namespace hill {
 		literal_values values;
 		std::vector<instr> instrs;
 
+		instr last()
+		{
+			if (instrs.size()<1) throw semantic_error_exception();
+			return instrs[instrs.size()-1];
+		}
+
+		instr second_last()
+		{
+			if (instrs.size()<2) throw semantic_error_exception();
+			return instrs[instrs.size()-2];
+		}
+
 		void add(const token &t)
 		{
 			switch (t.get_type()) {
@@ -53,14 +68,10 @@ namespace hill {
 				}
 				break;
 			case tt::OP_COLON_EQ:
-				if (instrs.size()<2) throw semantic_error_exception();
-				size_t left_ix = instrs.size()-2;
-				size_t right_ix = instrs.size()-1;
-
 				const auto &res_type = convert_binary(
 					t.get_type(),
-					instrs[left_ix].dt,
-					instrs[right_ix].dt);
+					second_last().dt,
+					last().dt);
 
 				// Bind id instruction on left side to the value (possibly converting to new type)
 				
