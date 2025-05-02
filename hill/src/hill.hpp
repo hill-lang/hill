bd5dd2e047302d8ba0c4b827d93fc264d211be50
Arild Hystad <arild.hystad@gmail.com>
2025-05-02 09:54:46 +0200

    Removed unnecessary generic types

--- hill/src/hill.hpp
+++ hill/src/hill.hpp
@@ -5,32 +5,33 @@
 
 #include <concepts>
 #include <iostream>
+#include <vector>
 
 namespace hill {
 
 	namespace concepts {
 		template<typename PT, typename LFT>
-		concept parser = requires(PT p, std::istream & istr, LFT lexer) {
-			{ p.parse(istr, lexer) };
-			{ p.get_rpn() };
+		concept parser = requires(PT p, std::istream &istr, LFT lexer) {
+			{p.parse(istr, lexer)};
+			{p.get_rpn()};
 		};
 
-		template<typename AT, typename RPNT>
-		concept analyzer = requires(AT a, RPNT rpn) {
-			{ a.analyze(rpn) };
-			{ a.get_main_block() };
+		template<typename AT>
+		concept analyzer = requires(AT a, std::vector<token> rpn) {
+			{a.analyze(rpn)};
+			{a.get_main_block()};
 		};
 
-		template<typename ET, typename BT>
-		concept evaluator = requires(ET e, BT b) {
-			{ e.evaluate(b) };
+		template<typename ET>
+		concept evaluator = requires(ET e, block b) {
+			{e.evaluate(b)};
 		};
 	}
 
 	template<typename LFT, typename PT, typename AT, typename ET>
-	requires concepts::parser<PT, LFT> &&
-		concepts::analyzer<AT, decltype(std::declval<PT>().get_rpn())> &&
-		concepts::evaluator<ET, decltype(std::declval<AT>().get_main_block())>
+	requires concepts::parser<PT, LFT>
+		&& concepts::analyzer<AT>
+		&& concepts::evaluator<ET>
 	void hill(std::istream &istr, LFT lexer, PT &parser, AT &analyzer, ET &evaluator)
 	{
 		parser.parse(istr, lexer);
