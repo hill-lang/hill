4db0a11c9b4327fb7a339cbea25b4adcc970d6e9
Arild Hystad <arild.hystad@gmail.com>
2025-05-13 15:42:23 +0200

    Type system structuring

--- hill/src/type.hpp
+++ hill/src/type.hpp
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <vector>
+#include <numeric>
 
 namespace hill {
 
@@ -12,7 +13,8 @@ namespace hill {
 		I, I8, I16, I32, I64,
 		U, U8, U16, U32, U64,
 		F, F32, F64, /*F128,*/
-		TUPLE,
+		ARRAY,
+		END,
 	};
 
 	constexpr size_t basic_type_size(basic_type bt)
@@ -39,30 +41,63 @@ namespace hill {
 	constexpr const char *basic_type_str(basic_type bt)
 	{
 		switch (bt) {
-		case basic_type::UNDECIDED: return "UNDECIDED";
-		case basic_type::STR: return "STR";
-		case basic_type::I: return "I";
-		case basic_type::I8: return "I8";
-		case basic_type::I16: return "I16";
-		case basic_type::I32: return "I32";
-		case basic_type::I64: return "I64";
-		case basic_type::U: return "U";
-		case basic_type::U8: return "U8";
-		case basic_type::U16: return "U16";
-		case basic_type::U32: return "U32";
-		case basic_type::U64: return "U64";
-		case basic_type::F: return "F";
-		case basic_type::F32: return "F32";
-		case basic_type::F64: return "F64";
-		//case basic_type::F128: return "F128";
+		case basic_type::UNDECIDED: return "@undecided";
+		case basic_type::STR: return "@str";
+		case basic_type::I: return "@i";
+		case basic_type::I8: return "@i8";
+		case basic_type::I16: return "@i16";
+		case basic_type::I32: return "@i32";
+		case basic_type::I64: return "@i64";
+		case basic_type::U: return "@u";
+		case basic_type::U8: return "@u8";
+		case basic_type::U16: return "@u16";
+		case basic_type::U32: return "@u32";
+		case basic_type::U64: return "@u64";
+		case basic_type::F: return "@f";
+		case basic_type::F32: return "@f32";
+		case basic_type::F64: return "@f64";
+		//case basic_type::F128: return "@f128";
+		case basic_type::ARRAY: return "@array";
+		case basic_type::END: return ")";
 		default: return "<UNKNOWN>";
 		}
 	}
 
+	struct type_spec {
+		type_spec() {}
+		type_spec(basic_type bt): types{bt} {}
+		type_spec(const std::vector<basic_type> &bts): types(bts) {}
+		type_spec(const type_spec &other): types(other.types) {}
+		bool operator==(const type_spec &other) const {return this->types == other.types;}
+		bool operator<(const type_spec &other) const {return this->types < other.types;}
+
+		std::vector<basic_type> types;
+
+		std::string to_str() const
+		{
+			std::ostringstream ss;
+
+			ss << '(';
+			for (auto &bt: types) {
+				ss << basic_type_str(bt);
+			}
+			return ss.str();
+		}
+
+		size_t size() const
+		{
+			return std::accumulate(
+				types.begin(),
+				types.end(),
+				(size_t)0,
+				[](size_t sum, auto &bt){return sum+basic_type_size(bt);});
+		}
+	};
+
 	struct data_type {
 		data_type(): bt(basic_type::UNDECIDED), mut(false) {}
 		data_type(basic_type bt): bt(bt), mut(false) {}
-		data_type(std::vector<data_type> tuple_types): bt(basic_type::TUPLE), tuple_types(tuple_types), mut(false) {}
+		//data_type(std::vector<data_type> tuple_types): bt(basic_type::TUPLE), tuple_types(tuple_types), mut(false) {}
 		data_type(const data_type &other): bt(other.bt), tuple_types(other.tuple_types), mut(other.mut) {}
 		bool operator==(const data_type &other) {return this->bt==other.bt;}
 		bool operator<(const data_type &other) const
