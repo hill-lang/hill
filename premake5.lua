93921465c6a0618e9e3e715c49a775d78e4897f5
Jonas Almaas <jonas.f.almaas@gmail.com>
2025-05-23 21:04:29 +0200

    Flattened project structure

--- premake5.lua
+++ premake5.lua
@@ -8,4 +8,34 @@ workspace "hill"
 		"Dist",
 	}
 
-	include "hill"
+project "hill"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++20"
+
+	targetdir "%{wks.location}/bin/%{cfg.buildcfg}"
+	objdir "%{wks.location}/bin-int/%{cfg.buildcfg}"
+
+	files {
+		"src/**.hpp",
+		"src/**.cpp",
+	}
+
+	includedirs {
+		"src",
+	}
+
+	filter "configurations:Debug"
+		defines "HILL_DEBUG"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines "HILL_RELEASE"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		defines "HILL_DIST"
+		runtime "Release"
+		optimize "on"
