ec6a6690ae025fd9dc08405c18cda90f83c6130d
Arild Hystad <arild.hystad@gmail.com>
2025-05-23 18:41:43 +0200

    Added parser tests

--- hill/src/test/parser.hpp
+++ hill/src/test/parser.hpp
@@ -14,6 +14,8 @@ namespace hill {
 			{"1", "NUM(1),END()"},
 			{"a", "NAME(a),END()"},
 			{"2 + 3", "NUM(2),NUM(3),OP_PLUS(+):Binary,END()"},
+			{"1 + 2 * 3", "NUM(1),NUM(2),NUM(3),OP_STAR(*):Binary,OP_PLUS(+):Binary,END()"},
+			{"(1 + 2) * 3", "NUM(1),NUM(2),OP_PLUS(+):Binary,RPAR()),NUM(3),OP_STAR(*):Binary,END()"},
 		};
 
 		inline void parser()
