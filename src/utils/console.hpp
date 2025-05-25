dca320308fd05fa71ec0b4243c69b2a52f537559
Jonas Almaas <jonas.f.almaas@gmail.com>
2025-05-25 13:54:21 +0200

    Console utils cleanup

--- src/utils/console.hpp
+++ src/utils/console.hpp
@@ -17,34 +17,34 @@ namespace hill::utils {
 		WHITE = 37, BRIGHT_WHITE = 97,
 	};
 
-	inline int current_color = 0;
-
-	inline std::ostream &apply_color(std::ostream &os) {
-		return os << "\033[" << current_color << "m";
+	/// <summary>
+	/// Set the conosle forground color
+	/// </summary>
+	inline std::string setcolor(ccolor c)
+	{
+		std::stringstream ss;
+		ss << "\033[" << static_cast<int>(c) << "m";
+		return ss.str();
 	}
 
 	/// <summary>
 	/// Set the conosle forground color
 	/// </summary>
-	inline std::ostream &(*setcolor(ccolor c))(std::ostream &) {
-		current_color = static_cast<int>(c);
-		return apply_color;
-	}
-
-	/// <summary>
-	/// Set the conosle forground color
-	/// </summary>
-	inline std::ostream &(*setbgcolor(ccolor c))(std::ostream &) {
-		current_color = static_cast<int>(c) + 10;
-		return apply_color;
+	inline std::string setbgcolor(ccolor c)
+	{
+		std::stringstream ss;
+		ss << "\033[" << static_cast<int>(c)+10 << "m";
+		return ss.str();
 	}
 
 	/// <summary>
 	/// Resets the conosle forground and background color
 	/// </summary>
-	inline std::ostream &(*resetcolor())(std::ostream &) {
-		current_color = static_cast<int>(ccolor::NONE);
-		return apply_color;
+	inline std::string resetcolor()
+	{
+		std::stringstream ss;
+		ss << "\033[" << static_cast<int>(ccolor::NONE) << "m";
+		return ss.str();
 	}
 
 	inline std::string escape_string(const char *s)
