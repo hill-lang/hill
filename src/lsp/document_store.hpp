c3a48b5c39bd291e10c9cf2f4dba857627e5c84b
Jonas Almaas <jonas.f.almaas@gmail.com>
2025-06-06 09:25:40 +0200

    Use string instead of filesystem::path since it doesnt have hash

--- src/lsp/document_store.hpp
+++ src/lsp/document_store.hpp
@@ -1,7 +1,6 @@
 #ifndef HILL__LSP__DOCUMENT_STORE_HPP_INCLUDED
 #define HILL__LSP__DOCUMENT_STORE_HPP_INCLUDED
 
-#include <filesystem>
 #include <string>
 #include <unordered_map>
 #include <mutex>
@@ -12,26 +11,26 @@ namespace hill::lsp {
 	struct document_store {
 		document_store() = default;
 
-		bool exists(const std::filesystem::path &fpath)
+		bool exists(const std::string &fpath)
 		{
 			std::lock_guard<std::mutex> guard(access_mutex);
 			return store.contains(fpath);
 		}
 
-		std::optional<std::string> get(const std::filesystem::path &fpath)
+		std::optional<std::string> get(const std::string &fpath)
 		{
 			std::lock_guard<std::mutex> guard(access_mutex);
 			if (!store.contains(fpath)) return std::nullopt;
 			else return store.at(fpath);
 		}
 
-		void set(const std::filesystem::path &fpath, const std::string &content)
+		void set(const std::string &fpath, const std::string &content)
 		{
 			std::lock_guard<std::mutex> guard(access_mutex);
 			store[fpath] = content;
 		}
 
-		bool remove(const std::filesystem::path &fpath)
+		bool remove(const std::string &fpath)
 		{
 			std::lock_guard<std::mutex> guard(access_mutex);
 			if (!store.contains(fpath)) return false;
@@ -42,7 +41,7 @@ namespace hill::lsp {
 
 	private:
 		std::mutex access_mutex;
-		std::unordered_map<std::filesystem::path, std::string> store;
+		std::unordered_map<std::string, std::string> store;
 	};
 }
 
