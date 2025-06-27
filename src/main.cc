2d6247a40bb726a315c3cc423ff1a5004d355db4
Jonas Almaas <jonas.f.almaas@gmail.com>
2025-06-27 17:38:51 +0200

    m

--- src/main.cc
+++ src/main.cc
@@ -40,45 +40,45 @@ int main(int argc, char *argv[])
 		if (!strcmp(argv[1], "run")) {
 			// TODO: Run file
 		} else if (!strcmp(argv[1], "fmt")) {
-			hill::fmt::formatter fmt;
+			::hill::fmt::formatter fmt;
 			// TODO: Handle flags
 			// TODO: Maybe allow formatting whatever is on stdin and printing it to stdout?
 			for (int i=2; i<argc; ++i) {
 				fmt.format(argv[i]);
 			}
 		} else if (!strcmp(argv[1], "lsp")) {
-			hill::lsp::server server;
+			::hill::lsp::server server;
 			server.run();
 		} else if (!strcmp(argv[1], "repl")) {
 			// TODO: REPL
 		} else if (!strcmp(argv[1], "test")) {
 			// TODO: Maybe make junit optional?
 			// TODO: Maybe allow setting output file
-			auto test_session = hill::utils::junit_session("Hill unit tests", "test-results.xml");
+			auto test_session = ::hill::utils::junit_session("Hill unit tests", "test-results.xml");
 			if (argc>2) {
-				if (!strcmp(argv[2], "lexer")) {ok = hill::test::lexer(test_session);}
-				else if (!strcmp(argv[2], "parser")) {ok = hill::test::parser(test_session);}
+				if (!strcmp(argv[2], "lexer")) {ok = ::hill::test::lexer(test_session);}
+				else if (!strcmp(argv[2], "parser")) {ok = ::hill::test::parser(test_session);}
 				//else if (!strcmp(argv[2], "analyzer")) {/*ok = hill::test::analyzer(test_session); */ }
-				else if (!strcmp(argv[2], "evaluator")) {ok = hill::test::evaluator(test_session);}
-				else if (!strcmp(argv[2], "json_parser")) {ok = hill::test::json_parser(test_session);}
+				else if (!strcmp(argv[2], "evaluator")) {ok = ::hill::test::evaluator(test_session);}
+				else if (!strcmp(argv[2], "json_parser")) {ok = ::hill::test::json_parser(test_session);}
 				else {return usage(argv[0]);}
 			} else {
-				if (!hill::test::lexer(test_session)) ok =false;
-				if (!hill::test::parser(test_session)) ok = false;
-				//if (!hill::test::analyzer(test_session)) ok = false;
-				if (!hill::test::evaluator(test_session)) ok = false;
-				if (!hill::test::json_parser(test_session)) ok = false;
+				if (!::hill::test::lexer(test_session)) ok =false;
+				if (!::hill::test::parser(test_session)) ok = false;
+				//if (!::hill::test::analyzer(test_session)) ok = false;
+				if (!::hill::test::evaluator(test_session)) ok = false;
+				if (!::hill::test::json_parser(test_session)) ok = false;
 			}
 			std::cout << '\n';
 			::hill::test::test_report(test_session, std::cout);
 		}
 	} else {
-		auto test_session = hill::utils::junit_session("Hill unit tests", "test-results.xml");
-		//if (!hill::test::lexer(test_session)) ok = false;
-		//if (!hill::test::parser(test_session)) ok = false;
-		//if (!hill::test::analyzer(test_session)) ok = false;
-		if (!hill::test::evaluator(test_session)) ok = false;
-		if (!hill::test::json_parser(test_session)) ok = false;
+		auto test_session = ::hill::utils::junit_session("Hill unit tests", "test-results.xml");
+		//if (!::hill::test::lexer(test_session)) ok = false;
+		//if (!::hill::test::parser(test_session)) ok = false;
+		//if (!::hill::test::analyzer(test_session)) ok = false;
+		if (!::hill::test::evaluator(test_session)) ok = false;
+		if (!::hill::test::json_parser(test_session)) ok = false;
 		std::cout << '\n';
 		::hill::test::test_report(test_session, std::cout);
 	}
